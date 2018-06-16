301750e562779240299c438403a8d1020c169b51
Mohammed-AB <31482527+Mohammed-AB@users.noreply.github.com>
2018-06-16 16:19:21 -0400

    first commit

---
+++ src/components/Name.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Titles = () => (
+
+	<div>
+		<h1 className="title-container__title">What is the Weathe Today</h1>
+	</div>
+
+);
+
+export default Titles;
+
+
+
+// <div id='stars'></div>
+//         <div id='stars2'></div>
+//         <div id='stars3'></div>
